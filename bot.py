88a2f7e94fa0408dee82bbb24029ec5bd80ab0a2
pdonnelly <pdonnelly@shutterstock.com>
2020-10-05 12:01:32 +0100

    Update Pbot to V2

--- bot.py
+++ bot.py
@@ -1,38 +1,35 @@
-#!/usr/bin/env python3
+import sys
+import time, selenium
+import colorama
+from colorama import Fore, Style
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from WebScraper import getItem
-import time
 
 def details():
     info = {}
-    l = ["Name", "Email", "Tel", "Ad1", "Ad2", "Ad3", "City", "ZIP", "Loc", "Card", "Month", "Year", "CVV"]
+    l = ["Name", "Email", "Phone", "Ad1", "Ad2", "Ad3", "City", "ZIP", "Country", "Card Number", "Card Month", "Card Year", "CVV"]
     i = 0
     while i < 13:
-        print(l[i] + ": ")
+        print(Fore.LIGHTBLUE_EX + l[i] + ": " + Fore.LIGHTGREEN_EX + Style.RESET_ALL)
         info[l[i]] = input()
         i += 1
     return info
 
 def main():
-    
-    print('Category: ')
+    # Input item info
+    print(Fore.MAGENTA + "Choose Category:" + Fore.CYAN + "jackets, shirts, top/sweaters, sweatshirts, pants, shorts, hats, bags, accessories, shoes, skate" + Fore.GREEN)
     category = input()
-    print('Item Name: ')
-    item = input()
-    print('Item Color: ')
+    print(Fore.MAGENTA + "Item Name: " + Fore.GREEN)
+    name = input()
+    print(Fore.MAGENTA + "Item Color: " + Fore.GREEN)
     color = input()
 
-    info = details()
     t0 = time.time()
-    browser = webdriver.Chrome('/home/philip/Downloads/chromedriver')
-    check = True
-    while check:
-        try:
-            item = 'https://www.supremenewyork.com' + getItem(category, item, color)
-            check = False
-        except:
-            continue
+    info = details()
+
+    item = 'https://www.supremenewyork.com' + getItem(category, name, color)
+    browser = webdriver.Chrome('/home/mark/Downloads/chromedriver')
     browser.get(item)
 
     browser.find_element_by_xpath('//*[@id="add-remove-buttons"]/input').click()
@@ -44,27 +41,28 @@ def main():
         except:
             continue
 
+    # User info
     browser.find_element_by_xpath('//*[@id="order_billing_name"]').send_keys(info["Name"])
     browser.find_element_by_xpath('//*[@id="order_email"]').send_keys(info["Email"])
-    browser.find_element_by_xpath('//*[@id="order_tel"]').send_keys(info["Tel"])
+    browser.find_element_by_xpath('//*[@id="order_tel"]').send_keys(info["Phone"])
     browser.find_element_by_xpath('//*[@id="bo"]').send_keys(info["Ad1"])
     browser.find_element_by_xpath('//*[@id="oba3"]').send_keys(info["Ad2"])
     browser.find_element_by_xpath('//*[@id="order_billing_address_3"]').send_keys(info["Ad3"])
     browser.find_element_by_xpath('//*[@id="order_billing_city"]').send_keys(info["City"])
     browser.find_element_by_xpath('//*[@id="order_billing_zip"]').send_keys(info["ZIP"])
-    browser.find_element_by_xpath('//*[@id="order_billing_country"]').send_keys(info["Loc"])
-    browser.find_element_by_xpath('//*[@id="cnb"]').send_keys(info["Card"])
-    browser.find_element_by_xpath('//*[@id="credit_card_month"]').send_keys(info["Month"])
-    browser.find_element_by_xpath('//*[@id="credit_card_year"]').send_keys(info["Year"])
+    browser.find_element_by_xpath('//*[@id="order_billing_country"]').send_keys(info["Country"])
+    browser.find_element_by_xpath('//*[@id="cnb"]').send_keys(info["Card Number"])
+    browser.find_element_by_xpath('//*[@id="credit_card_month"]').send_keys(info["Card Month"])
+    browser.find_element_by_xpath('//*[@id="credit_card_year"]').send_keys(info["Card Year"])
     browser.find_element_by_xpath('//*[@id="vval"]').send_keys(info["CVV"])
+
     browser.find_element_by_xpath('//*[@id="cart-cc"]/fieldset/p/label/div/ins').click()
     browser.find_element_by_xpath('//*[@id="pay"]/input').click()
 
     t1 = time.time()
     print(t1-t0)
-    chrome_options.add_experimental_option("detach", True)
 
-
+    time.sleep(7200)
 
 if __name__ == "__main__":
     main()
